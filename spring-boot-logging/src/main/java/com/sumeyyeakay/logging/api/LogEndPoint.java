ddc775c493a51b8e0bc7f46cd57465fac3cccf79
sümeyye akay <sumeyyeakayy@gmail.com>
2020-08-26 17:00:42 +0300

    spring boot logging fix

--- spring-boot-logging/src/main/java/com/sumeyyeakay/logging/api/LogEndPoint.java
+++ spring-boot-logging/src/main/java/com/sumeyyeakay/logging/api/LogEndPoint.java
@@ -1,5 +1,6 @@
 package com.sumeyyeakay.logging.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,17 +8,30 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author Sumeyye Akay
  * logging will be in this class.
- *
- *  Loglama -> Asenkron olmalidir. Bunu yapmazsak uygulama performansi biraz kaybedilmis olur.
- *  Request boylece gecikmemis olacaktir.
- */
+*/
 
+@Slf4j
 @RestController
 @RequestMapping("/log")
-public class LogEndPoint {
+public class LogEndPoint
+{
+
 
     @GetMapping
-    public String getDetails(){
-        return "Messages";
+    public String getDetails()
+    {
+        log.debug("get details method started");
+        return internalLogDetail();
+    }
+
+    private String internalLogDetail(){
+        try {
+            log.debug("get details method started");
+            Thread.sleep(1000);
+            return "API Messages";
+        } catch (InterruptedException interruptedException) {
+            log.error("Error : {}", interruptedException);
+        }
+        return "";
     }
 }
