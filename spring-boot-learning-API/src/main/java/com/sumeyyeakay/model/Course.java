9347b520ec48cd0d0b5b4cbc938cbf908bb2c3cc
sümeyye akay <sumeyyeakayy@gmail.com>
2020-08-27 22:13:40 +0300

    Course fix

--- spring-boot-learning-API/src/main/java/com/sumeyyeakay/model/Course.java
+++ spring-boot-learning-API/src/main/java/com/sumeyyeakay/model/Course.java
@@ -20,7 +20,48 @@ public class Course {
     private Topic topic;
 
 
+    public Course() {
+    }
 
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
 
+    public Course(String id, String name, String description, String topicId) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.topic = new Topic(topicId, "", "");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
 
 }
