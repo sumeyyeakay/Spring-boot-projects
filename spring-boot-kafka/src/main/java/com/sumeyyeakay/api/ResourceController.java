4c2a3308ace4df56272ba59693e8ba99039ee8a1
sümeyye akay <sumeyyeakayy@gmail.com>
2020-08-27 10:52:16 +0300

    ResourceController fix

--- spring-boot-kafka/src/main/java/com/sumeyyeakay/api/ResourceController.java
+++ spring-boot-kafka/src/main/java/com/sumeyyeakay/api/ResourceController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ResourceController {
 
-    @Value("${haydikodlayalim.kafka.topic}")
+    @Value("${sumeyyeakay.kafka.topic}")
     private String topic;
 
     private final KafkaTemplate<String, KMessage> kafkaTemplate;
